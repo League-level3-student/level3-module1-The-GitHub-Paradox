290a724326176230e1024c1d6c773175ebeeca10
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-12-31 10:41:47 -0800

    e
    e

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,20 +2,21 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private Integer[] Int;
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		Int = new Integer[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		Integer[] Int2 = new Integer[v+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		Int2[v+1] = Int2[1];
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		Int[v] = Int2[v];
 		//D. set the member array equal to the new array.
 		
 	}
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,9 +9,14 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> DBs = new Stack<Double>();
+		Random ran = new Random();
+		double randomnum = ran.nextDouble();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for(int i = 0; i > 100; i++) {
+			DBs.push(randomnum);
+			System.out.println(DBs.pop());
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
